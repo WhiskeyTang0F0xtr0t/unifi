68eded5609813548e0809a80489229513fae7226
WhiskeyTang0F0xtr0t <9803191+WhiskeyTang0F0xtr0t@users.noreply.github.com>
2024-02-19 14:40:22 -0500

    Update wtf-check-wpasupp.sh
    Fixed a lot of "bad practices" and changed the output formatting.

--- scripts/wtf-check-wpasupp.sh
+++ scripts/wtf-check-wpasupp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ####################################### 
-# wtf-check-wpasupp v2.1
+# wtf-check-wpasupp v2.5
 #
 # Checks the wpa_supplicant service and confirms needed files are present by parsing the existing config.
 #
@@ -15,30 +15,31 @@ log_file="${short_filename}.log"
 ####################################### 
 log() {
 	local flag="$1"; shift
-	stamp=`date '+[%F %T]'`
+	stamp=$(date '+[%F %T]')
 	case $flag in
-		I) echo "$stamp - INFO: ${@}" >> $log_file ;;
-		IF) echo "$stamp - INFO: Found - ${@}" >> $log_file ;;
-		IP) echo "$stamp - INFO: Parsed - ${@}" >> $log_file ;;
-		E) echo "$stamp - ERROR: ${@}" >> $log_file ;;
-		ENF) echo "$stamp - ERROR: Not found - ${@}" >> $log_file ;;
-		B) echo "$stamp - ${@}"  >> $log_file ;;
+		I) echo "$stamp - INFO: ${*}" >> "$log_file" ;;
+		IF) echo "$stamp - INFO: Found - ${*}" >> "$log_file" ;;
+		IP) echo "$stamp - INFO: Parsed - ${*}" >> "$log_file" ;;
+		E) echo "$stamp - ERROR: ${*}" >> "$log_file" ;;
+		ENF) echo "$stamp - ERROR: Not found - ${*}" >> "$log_file" ;;
+		B) echo "$stamp - ${*}"  >> "$log_file" ;;
 	esac
 }
 
 log_stream() {
   # used to capture stream output from command responses
-  [[ ! -t 0 ]] && while read line; do echo "$(date '+[%F %T]') $line" >> $log_file; done
+  [[ ! -t 0 ]] && while read -r line; do echo "$(date '+[%F %T]') $line" >> "$log_file"; done
 }
-
-# Banner column formatting
-format="%20s : %-s\n"
 
 # Output colors
 CYAN=$(tput setaf 6)
 GREEN=$(tput setaf 2)
 RED=$(tput setaf 1)
 NC=$(tput sgr0)
+TICK="[${GREEN}✓${NC}]"
+CROSS="[${RED}✗${NC}]"
+INFO="[i]"
+DONE="${GREEN} done!${NCNC}"
 
 #######################################
 # Show a formatted banner with message
@@ -47,10 +48,15 @@ NC=$(tput sgr0)
 # RETURNS:
 #   Writes string to stdout
 ####################################### 
+#banner () {
+#	local string=$1
+#	padding="============================================================"
+#	printf "%s\n" "== ""$string"" ""${padding:${#string}}" && log B "*** $string ***"
+#}
+
 banner () {
 	local string=$1
-	padding="============================================================"
-	printf "%s\n" "== ""$string"" ""${padding:${#string}}" && log B "*** $string ***"
+	printf "%b %s\\n" "${INFO}" "${string}"
 }
 
 #######################################
@@ -59,11 +65,13 @@ banner () {
 #   Status message, exits script if fails
 #######################################
 check-hw () {
+  # The message to display when this function is running
 	if command -V ubnt-device-info 1> /dev/null 2> >(log_stream); then
-		local model=`ubnt-device-info model`
-		printf "$format" "Hardware" "${CYAN}${model}${NC}"; log I "Hardware - ${model}"
+		local model
+		model=$(ubnt-device-info model)
+		printf "   %b  %b %s\\n" "${TICK}" "Model:" "${CYAN}${model}${NC}"; log I "Hardware - ${model}"
 	else
-      printf "$format" "Hardware" "${RED}UNSUPPORTED HARDWARE - EXITING${NC}"; log E "UNSUPPORTED HARDWARE - EXITING"; exit 1
+    printf "   %b %s\\n" "${CROSS}" "${RED}UNSUPPORTED HARDWARE - EXITING${NC}"; log E "UNSUPPORTED HARDWARE - EXITING"; exit 1
 	fi
 }
 
@@ -78,10 +86,10 @@ parse-wan-int () {
 # Checks ubios-udapi-server.state to determine the WAN port
 	if [ -f /data/udapi-config/ubios-udapi-server/ubios-udapi-server.state ]; then
 		# Parses udapi-net-cfg.json and etracts first interface in wanFailover yaml object
-		udapi_wan_int=`jq -r '.services.wanFailover.wanInterfaces.[0].interface' /data/udapi-config/udapi-net-cfg.json`
-		printf "$format" "WAN Interface" "${CYAN}${udapi_wan_int}${NC}" && log I "WAN Interface: ${udapi_wan_int}"
+		udapi_wan_int=$(jq -r '.services.wanFailover.wanInterfaces.[0].interface' /data/udapi-config/udapi-net-cfg.json)
+		printf "   %b  %b %s\\n" "${TICK}" "WAN Int:" "${CYAN}${udapi_wan_int}${NC}" && log I "WAN Interface: ${udapi_wan_int}"
 	else
-		printf "$format" "WAN Interface" "${RED}Could not determine WAN interface from udapi-net-cfg - EXITING${NC}"; log E "Could not determine WAN interface from udapi-net-cfg - EXITING"; exit 1
+		printf "   %b  %b %s\\n" "${CROSS}" "WAN Int:" "${RED}Could not determine WAN interface from udapi-net-cfg - EXITING${NC}"; log E "Could not determine WAN interface from udapi-net-cfg - EXITING"; exit 1
 	fi
 }
 
@@ -93,10 +101,10 @@ parse-wan-int () {
 check-wpa-supp-installed () {
 # Check if wpa_supplicant is installed with dpkg
 	if dpkg -s wpasupplicant 1> /dev/null 2> >(log_stream); then
-   	wpa_supp_ver=`dpkg -s wpasupplicant | grep -i '^Version' | cut -d' ' -f2`
-	   printf "$format" "wpa_supplicant" "${GREEN}Installed: ${wpa_supp_ver}${NC}" && log I "wpa_supplicant installed: ${wpa_supp_ver}"
+   	wpa_supp_ver=$(dpkg -s wpasupplicant | grep -i '^Version' | cut -d' ' -f2)
+	   printf "   %b  %b %s\\n" "${TICK}" "Installed:" "${GREEN}${wpa_supp_ver}${NC}" && log I "wpa_supplicant installed: ${wpa_supp_ver}"
 	else
-	   printf "$format" "wpa_supplicant" "${RED}NOT INSTALLED${NC}"; log E "wpa_supplicant not installed"; return 1
+	   printf "   %b  %b %s\\n" "${CROSS}" "Installed:" "${RED}NOT INSTALLED${NC}"; log E "wpa_supplicant not installed"; return 1
 	fi
 }
 
@@ -108,9 +116,9 @@ check-wpa-supp-installed () {
 check-wpa-supp-active () {
 # Check if wpa_supplicant is active with systemctl
 	if systemctl is-active wpa_supplicant 1> /dev/null 2> >(log_stream); then
-	   printf "$format" "wpa_supplicant" "${GREEN}Service is ACTIVE${NC}" && log I "wpa_supplicant is active"
+	   printf "   %b  %b %s\\n" "${TICK}" "Active:" "${GREEN}Yes${NC}" && log I "wpa_supplicant is active"
 	else
-	   printf "$format" "wpa_supplicant" "${RED}NOT ACTIVE${NC}"; log E "wpa_supplicant is not active"
+	   printf "   %b  %b %s\\n" "${CROSS}" "Active:" "${RED}No${NC}"; log E "wpa_supplicant is not active"
 	fi
 }
 
@@ -122,9 +130,9 @@ check-wpa-supp-active () {
 check-wpa-supp-enabled () {
 # Check if wpa_supplicant is active with systemctl
 	if systemctl is-enabled wpa_supplicant 1> /dev/null 2> >(log_stream); then
-	   printf "$format" "wpa_supplicant" "${GREEN}Service is ENABLED${NC}" && log I "wpa_supplicant is enabled"
+	   printf "   %b  %b %s\\n" "${TICK}" "Enabled:" "${GREEN}Yes${NC}" && log I "wpa_supplicant is enabled"
 	else
-	   printf "$format" "wpa_supplicant" "${RED}NOT ENABLED${NC}"; log E "wpa_supplicant is not enabled"
+	   printf "   %b  %b %s\\n" "${CROSS}" "Enabled:" "${RED}No${NC}"; log E "wpa_supplicant is not enabled"
 	fi
 }
 
@@ -137,11 +145,12 @@ check-wpa-supp-enabled () {
 ####################################### 
 check-for-override () {
 	if [ -f /etc/systemd/system/wpa_supplicant.service.d/override.conf ]; then
-	   printf "$format" "override.conf" "${GREEN}FOUND${NC}"; log IF "/etc/systemd/system/wpa_supplicant.service.d/override.conf"
-	   override_wpasupp_path=`awk '/Dwired/{ print $6 }' /etc/systemd/system/wpa_supplicant.service.d/override.conf | sed 's/-c//'`
-	   printf "$format" "override.conf" "Parsed - ${GREEN}${override_wpasupp_path}${NC}"; log IP "${override_wpasupp_path}"
+	   printf "   %b  %b %s\\n" "${TICK}" "override.conf:" "${GREEN}FOUND${NC}"; log IF "/etc/systemd/system/wpa_supplicant.service.d/override.conf"
+	   override_wpasupp_path=$(awk '/Dwired/{ print $6 }' /etc/systemd/system/wpa_supplicant.service.d/override.conf | sed 's/-c//')
+	   printf "   %b %s\\n" "${INFO}" "Parsing override.conf for service configuration file"
+	   printf "   %b  %b %s\\n" "${TICK}" "Parsed:" "${GREEN}${override_wpasupp_path}${NC}"; log IP "${override_wpasupp_path}"
 	else
-	   printf "$format" "override.conf" "${GREEN}NOT FOUND${NC}"; log E "override.conf was not found"
+	   printf "   %b  %b %s\\n" "${TICK}" "override.conf:" "${RED}NOT FOUND${NC}"; log E "override.conf was not found"
 	fi
 }
 
@@ -153,12 +162,12 @@ check-for-override () {
 #   Status message, exits script if fails
 #######################################
 parse_service_conf () {
-	wpasuppconf_longpath=`systemctl status wpa_supplicant --no-pager | awk '/Dwired/{ print $7 }' | sed 's/-c//'`
-	wpasuppconf_filename=`basename ${wpasuppconf_longpath}`
-	wpasuppconf_path=`dirname ${wpasuppconf_longpath}`
-   printf "$format" "wpa_supplicant" "Parsed - ${GREEN}${wpasuppconf_longpath}${NC}"; log IF "${wpasuppconf_longpath}"
-	wpasupp_int=`systemctl status wpa_supplicant --no-pager | awk '/Dwired/{ print $6 }' | sed 's/-i//'` && log IP "Dwired"
-   printf "$format" "wpa_supplicant" "Parsed - Interface: ${CYAN}${wpasupp_int}${NC}"; log IP "Interface: ${wpasupp_int}"
+	wpasuppconf_longpath=$(systemctl status wpa_supplicant --no-pager | awk '/Dwired/{ print $7 }' | sed 's/-c//')
+	wpasuppconf_filename=$(basename "${wpasuppconf_longpath}")
+	#wpasuppconf_path=$(dirname "${wpasuppconf_longpath}") #Unused
+   printf "   %b  %b %s\\n" "${TICK}" "Found:" "${GREEN}${wpasuppconf_longpath}${NC}"; log IF "${wpasuppconf_longpath}"
+	wpasupp_int=$(systemctl status wpa_supplicant --no-pager | awk '/Dwired/{ print $6 }' | sed 's/-i//') && log IP "Dwired"
+   printf "   %b  %b %s\\n" "${TICK}" "Found:" "${CYAN}${wpasupp_int}${NC}"; log IP "Interface: ${wpasupp_int}"
 }
 
 #######################################
@@ -169,27 +178,27 @@ parse_service_conf () {
 #   Status messages
 #######################################
 parse-wpasupp-conf () {
-	if [ -f ${wpasuppconf_longpath} ]; then
+	if [ -f "${wpasuppconf_longpath}" ]; then
 		log I "wpa_supplicant conf - ${wpasuppconf_longpath}"
-		inetONTmac=`awk -F '"' '/identity/{ print $2 }' ${wpasuppconf_longpath}` && log IP "identity"
-		printf "$format" "${wpasuppconf_filename}" "ONT MAC - ${CYAN}${inetONTmac}${NC}"; log IP "ONT MAC - ${inetONTmac}"
-		CA=`awk -F '"' '/ca_cert/{ print $2 }' ${wpasuppconf_longpath}` && log IP "ca_cert"
-		CA_filename=`basename ${CA}`
-		CA_path=`dirname ${CA}`
-		printf "$format" "${wpasuppconf_filename}" "CA Path - ${GREEN}${CA_path}${NC}"; log IP "CA Path: ${CA_path}"
-		printf "$format" "${wpasuppconf_filename}" "CA Filename - ${GREEN}${CA_filename}${NC}"; log IP "CA Filename: ${CA_filename}"
-		Client=`awk -F '"' '/client_cert/{ print $2 }' ${wpasuppconf_longpath}` && log IP "client_cert"
-		Client_filename=`basename ${Client}`
-		Client_path=`dirname ${Client}`
-		printf "$format" "${wpasuppconf_filename}" "Client Path - ${GREEN}${Client_path}${NC}"; log IP "Client Path: ${Client_path}"
-		printf "$format" "${wpasuppconf_filename}" "Client Filename - ${GREEN}${Client_filename}${NC}"; log IP "Client Filename: ${Client_filename}"
-		PrivateKey=`awk -F '"' '/private_key/{ print $2 }' ${wpasuppconf_longpath}` && log IP "private_key"
-		PrivateKey_filename=`basename ${PrivateKey}`
-		PrivateKey_path=`dirname ${PrivateKey}`
-		printf "$format" "${wpasuppconf_filename}" "PrivateKey Path - ${GREEN}${PrivateKey_path}${NC}"; log IP "PrivateKey Path: ${PrivateKey_path}"
-		printf "$format" "${wpasuppconf_filename}" "PrivateKey Filename - ${GREEN}${PrivateKey_filename}${NC}"; log IP "PrivateKey Filename: ${PrivateKey_filename}"
+		inetONTmac=$(awk -F '"' '/identity/{ print $2 }' "${wpasuppconf_longpath}") && log IP "identity"
+		printf "   %b  %b %s\\n" "${TICK}" "ONT MAC:" "${CYAN}${inetONTmac}${NC}"; log IP "ONT MAC - ${inetONTmac}"
+		CA=$(awk -F '"' '/ca_cert/{ print $2 }' "${wpasuppconf_longpath}") && log IP "ca_cert"
+		CA_filename=$(basename "${CA}")
+		CA_path=$(dirname "${CA}")
+		printf "   %b  %b %s\\n" "${TICK}" "CA Path:" "${GREEN}${CA_path}${NC}"; log IP "CA Path: ${CA_path}"
+		printf "   %b  %b %s\\n" "${TICK}" "CA Filename:" "${GREEN}${CA_filename}${NC}"; log IP "CA Filename: ${CA_filename}"
+		Client=$(awk -F '"' '/client_cert/{ print $2 }' "${wpasuppconf_longpath}") && log IP "client_cert"
+		Client_filename=$(basename "${Client}")
+		Client_path=$(dirname "${Client}")
+		printf "   %b  %b %s\\n" "${TICK}" "Client Path:" "${GREEN}${Client_path}${NC}"; log IP "Client Path: ${Client_path}"
+		printf "   %b  %b %s\\n" "${TICK}" "Client Filename:" "${GREEN}${Client_filename}${NC}"; log IP "Client Filename: ${Client_filename}"
+		PrivateKey=$(awk -F '"' '/private_key/{ print $2 }' "${wpasuppconf_longpath}") && log IP "private_key"
+		PrivateKey_filename=$(basename "${PrivateKey}")
+		PrivateKey_path=$(dirname "${PrivateKey}")
+		printf "   %b  %b %s\\n" "${TICK}" "PrivateKey Path:" "${GREEN}${PrivateKey_path}${NC}"; log IP "PrivateKey Path: ${PrivateKey_path}"
+		printf "   %b  %b %s\\n" "${TICK}" "PrivateKey Filename:" "${GREEN}${PrivateKey_filename}${NC}"; log IP "PrivateKey Filename: ${PrivateKey_filename}"
 	else
-	   printf "$format" "${wpasuppconf_filename}" "${RED}MISSING - ${wpasuppconf_longpath}${NC}"; log ENF "wpa_supplicant conf not found at the parsed path"
+	   printf "   %b  %b %s\\n" "${CROSS}" "${RED}MISSING" "${wpasuppconf_longpath}${NC}"; log ENF "wpa_supplicant conf not found at the parsed path"
 	fi
 }
 
@@ -202,12 +211,13 @@ parse-wpasupp-conf () {
 ####################################### 
 check-for-pems () {
 	local cert_type=(CA Client PrivateKey)
-	local cert_paths=(${CA} ${Client} ${PrivateKey})
+	local cert_paths=("${CA}" "${Client}" "${PrivateKey}")
+	local cert_files=("${CA_filename}" "${Client_filename}" "${PrivateKey_filename}")
 	for i in "${!cert_type[@]}"; do
-		if [ -f ${cert_paths[i]} ]; then
-		   printf "$format" "${cert_type[i]}" "${GREEN}Found ${cert_paths[i]}${NC}"; log IF "${cert_type[i]} ${cert_paths[i]}"
+		if [ -f "${cert_paths[i]}" ]; then
+		   printf "   %b  %b %s\\n" "${TICK}" "${cert_type[i]}:" "${GREEN}${cert_files[i]}${NC}"; log IF "${cert_type[i]} ${cert_files[i]}"
 		else
-		   printf "$format" "${cert_type[i]}" "${RED}MISSING ${cert_paths[i]}${NC}"; log ENF "${cert_type[i]} ${cert_paths[i]}"
+		   printf "   %b  %b %s\\n" "${CROSS}" "${cert_type[i]}:" "${RED}${cert_files[i]}${NC}"; log ENF "${cert_type[i]} ${cert_files[i]}"
 		fi
 	done
 }
@@ -220,12 +230,14 @@ check-for-pems () {
 ####################################### 
 check-compare-interfaces () {
 if [[ "$udapi_wan_int" == "$wpasupp_int" ]]; then
-	printf "$format" "Interfaces" "${GREEN}Match: Detected WAN - ${CYAN}${udapi_wan_int}${NC}"
-	printf "$format" "Interfaces" "${GREEN}Match: Service Conf - ${CYAN}${wpasupp_int}${NC}"
+	printf "   %b  %b %s\\n" "${INFO}" "Detected WAN" "${CYAN}${udapi_wan_int}${NC}"
+	printf "   %b  %b %s\\n" "${INFO}" "Service Conf" "${CYAN}${wpasupp_int}${NC}"
+	printf "   %b  %s\\n" "${TICK}" "Detected interface matches service conf file"
 	log I "Detected WAN interface matches wpa_supplicant service conf"
 else
-	printf "$format" "Interfaces" "${RED}Mismatch: Detected WAN - ${CYAN}${udapi_wan_int}${NC}"
-	printf "$format" "Interfaces" "${RED}Mismatch: Service Conf - ${CYAN}${wpasupp_int}${NC}"
+	printf "   %b  %b %s\\n" "${INFO}" "Detected WAN" "${CYAN}${udapi_wan_int}${NC}"
+	printf "   %b  %b %s\\n" "${INFO}" "Service Conf" "${CYAN}${wpasupp_int}${NC}"
+	printf "   %b  %s\\n" "${CROSS}" "Detected interface does not match wpa_supplicant service conf file"
 	log E "Detected WAN interface(ubios-udapi-server.state) does not match wpa_supplicant service conf file"
 fi
 }
@@ -234,8 +246,8 @@ fi
 ## Main script
 ####################################### 
 clear
-rm $log_file
-banner "Logging to: $log_file"
+rm "$log_file"
+#banner "Logging to: $log_file"
 
 banner "Checking Hardware"
 check-hw
@@ -250,7 +262,7 @@ check-wpa-supp-enabled
 banner "Checking for override.conf file"
 check-for-override
 
-banner "Parsing active config from wpa_supplicant service"
+banner "Pulling active config from wpa_supplicant service"
 parse_service_conf
 
 banner "Parsing ${wpasuppconf_filename}"
